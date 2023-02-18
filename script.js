0554d090eba28afd3dd51d1a2db41d29ade4fb2e
Elkhan911 <955pro@mail.ru>
2023-02-19 00:08:42 +0500

    tasks 3-4

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 // 1. Создайте объект класса Employee и выведите его в консоль.
 class Employee {}
-let employee = new Employee();
-console.log(employee);
+let employee1 = new Employee();
+
+console.log(employee1);
 
 // 2. В объект класса Employee запишите свойства name, age и salary.
+employee1.name = "Khan";
+employee1.age = 28;
+employee1.salary = 4000;
 
-employee.name = "Khan";
-employee.age = 28;
-employee.salary = 4000;
+console.log(employee1, employee1.name, employee1.age, employee1.salary);
 
-console.log(employee.name, employee.age, employee.salary);
+// 3. Создайте несколько объектов класса Employee.
+let employee2 = new Employee();
+employee2.name = "Raul";
+employee2.age = 30;
+employee2.salary = 6000;
+
+console.log(employee2);
+
+// 4. Выведите на экран сумму зарплат созданных вами работников.
+let sumEmployeeSalaries = employee1.salary + employee2.salary;
+
+console.log(sumEmployeeSalaries);
