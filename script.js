7170796dca96dec062b105406ca364f1ed56060c
Elkhan911 <955pro@mail.ru>
2023-02-18 23:52:10 +0500

    1-2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello");
+// 1. Создайте объект класса Employee и выведите его в консоль.
+class Employee {}
+let employee = new Employee();
+console.log(employee);
+
+// 2. В объект класса Employee запишите свойства name, age и salary.
+
+employee.name = "Khan";
+employee.age = 28;
+employee.salary = 4000;
+
+console.log(employee.name, employee.age, employee.salary);
