6c0ebc3d6e6d6e4cd4a94ab4200255d89f586806
Elkhan911 <955pro@mail.ru>
2023-06-20 01:09:26 +0500

    task 4

--- script.js
+++ script.js
@@ -5,10 +5,55 @@ class Employee {
   age;
 }
 
-let employee1 = new Employee();
-employee1.name = "Donald";
-employee1.age = 21;
+let employee = new Employee();
+employee.name = "Donald";
+employee.age = 21;
 
-console.log(employee1);
-console.log(employee1.name);
-console.log(employee1.age);
+console.log(employee);
+console.log(employee.name);
+console.log(employee.age);
+
+// 2.  Создайте объект класса Car и выведите его в консоль.
+
+class Car {
+  year;
+  country;
+  color;
+}
+
+let car = new Car();
+
+car.year = 1984;
+car.country = "England";
+car.color = "blue";
+
+console.log(car);
+console.log(car.country);
+
+// 3.  Создайте объект класса Book и выведите его в консоль.
+
+class Book {
+  author;
+  pages;
+}
+
+let book = new Book();
+
+book.author = "A. S. Pushkin";
+pages = 1200;
+
+console.log(book);
+console.log(book.author);
+
+// 4. Создайте несколько объектов класса Manager.
+
+class Manager {}
+
+let manager1 = new Manager();
+let manager2 = new Manager();
+
+manager1.name = "John";
+manager2.name = "Alex";
+
+console.log(manager1);
+console.log(manager2);
